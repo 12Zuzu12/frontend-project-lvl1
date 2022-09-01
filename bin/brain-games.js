84e99b171778964f46209c5eec499cc2859aa265
Radgab <radgab712@gmail.com>
2022-09-01 21:32:42 +0300

    Change brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
-import redName from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-redName();
+const greetings = () => {
+  console.log('Welcome to the Brain Games!');
+  
+}
+export default greetings;
