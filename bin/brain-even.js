5bb8b8471e44d35a88474c7e89a645424cc5fb67
Radgab <radgab712@gmail.com>
2022-09-05 16:06:18 +0300

    add brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,50 +4,34 @@ import greetings from "../bin/brain-games.js"
 import readlineSync from 'readline-sync';
 
 
-greetings()
+greetings();
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello,${name}!`);
 
-//export function getRandomInRange(min, max) {
-   // console.log(Math.floor(Math.random() * (max - min + 1)) + min);
-  //}
-  let correctYes = 0
-  console.log('Answer "yes" if the number is even, otherwise answer "no".')
-  while (true ) {
-    if(correctYes === 3 ) {
-        console.log(`"Congratulations, ${name}!"`)
-        break
-      }
-  const randomNumber = Math.floor(Math.random() * (10 - 1 + 1)) + 1
-  console.log(`Question: ${randomNumber}`)
+let correctYes = 0;
+console.log('Answer "yes" if the number is even, otherwise answer "no".')
+while (true) {
+  if (correctYes === 3) {
+    console.log(`"Congratulations, ${name}!"`);
+    break;
+  }
+  const randomNumber = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+  console.log(`Question: ${randomNumber}`);
   const  parityIssue = readlineSync.question(`Your answer: `);
 
   if (randomNumber % 2 === 0 && parityIssue === "yes" && correctYes <= 3) {
-   console.log("Correct!")
-   correctYes += 1
-   
-  }else if (randomNumber % 2 !== 0 && parityIssue === "no" && correctYes <= 3 ) {
-    console.log("Correct!")
-    correctYes += 1
-    
-  }else if (randomNumber % 2 === 0 && parityIssue === "no") {
-    console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.Let'/s try again, ${name} `)
-    break
-  }else if (randomNumber % 2 !== 0 && parityIssue === "yes") {
-    console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.Let's try again , ${name}`)
-    break
-  }else{
-    
-    break
+    console.log("Correct!");
+    correctYes += 1;
+  }else if (randomNumber % 2 !== 0 && parityIssue === "no" && correctYes <= 3) {
+    console.log("Correct!");
+    correctYes += 1;
+  } else if (randomNumber % 2 === 0 && parityIssue === "no") {
+    console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.Let'/s try again, ${name} `);
+    break;
+  } else if (randomNumber % 2 !== 0 && parityIssue === "yes") {
+    console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.Let's try again , ${name}`);
+    break;
+  } else {
+    break;
   }
-  
-    
-  }
-
-
-
-
-
-
-
-//console.log("Hello motherfaka")
+}
