9cd674d9bae54664e7bf7e63000fe28d168d66f7
Radgab <radgab712@gmail.com>
2022-09-13 18:21:09 +0300

    change  brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,48 +1,25 @@
 #!/usr/bin/env node
 import { getRndInteger } from '../randomFile.js';
-import {conclusions } from '../src/index.js'
+import { conclusions } from '../src/index.js';
 
 const getInstructionsGames = () => 'What number is missing in the progression?';
 
-export function prpgression () {
+const progression = () => {
+  let firstNumber = getRndInteger(100, 1);
+  let step = getRndInteger(10, 1);
+  let algebraicProgression = [firstNumber];
+  for (let i = 0; algebraicProgression.length-1 < 7; i += 1 ) {
+    algebraicProgression[i] = algebraicProgression[i] + step
+    let item = algebraicProgression[i];
+    algebraicProgression.push(item)
+  }
+  algebraicProgression.pop()
+  const point = '..';
+  let e = Math.floor(Math.random()*7) + 1;
+  let  correctNumber = algebraicProgression[e] 
+  algebraicProgression[e] = point
+  const question1 = `${algebraicProgression}`;
+  return [correctNumber,question1]
+};
 
-    let firstNumber = getRndInteger(100, 1);
-    let step = getRndInteger(10, 1);
-    //let firstNumber = 57;
-    //let step = 5;
-    
-  //  console.log(firstNumber, 'Цифра');
-   // console.log(step, 'шаг');
-    
-    let algebraicProgression = [firstNumber];
-   
-    for (let i = 0; algebraicProgression.length-1 < 7; i += 1 ) {
-       
-        algebraicProgression[i] = algebraicProgression[i] + step
-        let item = algebraicProgression[i];
-        algebraicProgression.push(item)
-    }
-    
-   algebraicProgression.pop()
-  
-  
-    const point = '..';
-    let e = Math.floor(Math.random()*7) + 1;
-    let  correctNumber = algebraicProgression[e] 
-    algebraicProgression[e] = point
-    //console.log(algebraicProgression)
-    
-    const question1 =  `Question: ${algebraicProgression}`;
-    
-  //  console.log(correctNumber)
-  
-   
-    return [correctNumber,question1]
-}
-
-//console.log(prpgression())
-
-
-
-conclusions(prpgression(),getInstructionsGames())
-
+conclusions(progression,getInstructionsGames());
