f7db0c6e23bd2a795c9e0551ee1f73a686c1fb8e
Radgab <radgab712@gmail.com>
2022-09-16 21:04:56 +0300

    add bin/brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,25 +1,7 @@
 #!/usr/bin/env node
-import { getRndInteger } from '../randomFile.js';
+import progression from '../src/games/progression.js';
 import { conclusions } from '../src/index.js';
 
 const getInstructionsGames = () => 'What number is missing in the progression?';
-
-const progression = () => {
-  let firstNumber = getRndInteger(100, 1);
-  let step = getRndInteger(10, 1);
-  let algebraicProgression = [firstNumber];
-  for (let i = 0; algebraicProgression.length-1 < 7; i += 1 ) {
-    algebraicProgression[i] = algebraicProgression[i] + step
-    let item = algebraicProgression[i];
-    algebraicProgression.push(item)
-  }
-  algebraicProgression.pop()
-  const point = '..';
-  let e = Math.floor(Math.random()*7) + 1;
-  let  correctNumber = algebraicProgression[e] 
-  algebraicProgression[e] = point
-  const question1 = `${algebraicProgression}`;
-  return [correctNumber,question1]
-};
-
-conclusions(progression,getInstructionsGames());
+progression();
+conclusions(progression, getInstructionsGames());
