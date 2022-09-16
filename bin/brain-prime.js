b23bbcf8ce279fa94a75129d7a5352ff4c6e8a10
Radgab <radgab712@gmail.com>
2022-09-16 21:02:28 +0300

    Update bin/brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,17 +1,8 @@
 #!/usr/bin/env node
 
-import { getRndInteger } from '../randomFile.js';
-import {conclusions } from '../src/index.js';
+import prime from '../src/games/prime.js';
+import { conclusions } from '../src/index.js';
 
 const getInstructionsGames = 'Answer "yes" if given number is prime. Otherwise answer "no"';
-const  prime = () => {
-  let correctAnswer = '';
-  let randomNumber = getRndInteger(100, 1)
-  if  ( randomNumber % 2 === 0 || randomNumber % 3 === 0 ) {
-    correctAnswer = 'no';
-  }else{
-    correctAnswer = 'yes';
-  }
-  return [correctAnswer,randomNumber] 
-};
+prime();
 conclusions(prime,getInstructionsGames);
