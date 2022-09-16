e8f26cd0f792238cceed3e6c0838ff4a3ad275ca
Radgab <radgab712@gmail.com>
2022-09-16 23:58:39 +0300

    update src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const evenGames = () => {
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello,${name}!`);
   let correctYes = 0;
