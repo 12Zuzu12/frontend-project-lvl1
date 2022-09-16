557d48550289533194de5c51476a8ee8b176281f
Radgab <radgab712@gmail.com>
2022-09-17 00:27:25 +0300

    fix src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -22,11 +22,11 @@ const evenGames = () => {
       correctYes += 1;
     } else if (randomNumber % 2 === 0 && parityIssue === 'no') {
       console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.
-      Let'/s try again, ${name}!`);
+Let'/s try again, ${name}!`);
       break;
     } else if (randomNumber % 2 !== 0 && parityIssue === 'yes') {
       console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.
-      Let's try again, ${name}!`);
+Let's try again, ${name}!`);
       break;
     } else {
       break;
