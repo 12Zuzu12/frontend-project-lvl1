639fedb21550d85276d41c63591f5ec3cdda26d1
Radgab <radgab712@gmail.com>
2022-09-17 09:27:40 +0300

    fix src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,7 +2,7 @@ import { getRndInteger } from '../Helper.js';
 
 const progression = () => {
   let firstNumber = getRndInteger(100, 1);
-  let step = getRndInteger(10, 1);
+  let step = getRndInteger(5, 1);
   let resultNumber = '';
   let algebraicProgression = [firstNumber];
   for (let i = 0; algebraicProgression.length-1 < 7; i += 1 ) {
@@ -15,9 +15,9 @@ const progression = () => {
   let e = Math.floor(Math.random()*7) + 1;
   let correctNumber = algebraicProgression[e];
   algebraicProgression[e] = point;
-  const question1 = `${algebraicProgression}`;
+  const question1 = `${algebraicProgression.join(' ')}`;
   resultNumber += correctNumber;
-  return [resultNumber,question1];
+  return [resultNumber, question1];
 };
 export default progression;
   
