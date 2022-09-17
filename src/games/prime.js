7b945cc1d37fbf654bf8fcefed932c37e9eb44d6
Radgab <radgab712@gmail.com>
2022-09-17 10:56:49 +0300

    fix ../src/games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,7 +4,7 @@ const prime = () => {
   let correctAnswer = '';
   // eslint-disable-next-line prefer-const
   let randomNumber = getRndInteger(100, 1);
-  if (randomNumber % 2 === 0 || randomNumber % 3 === 0) {
+  if (randomNumber % 2 === 0 || randomNumber % 3 === 0 || randomNumber % 5 === 0) {
     correctAnswer = 'no';
   } else {
     correctAnswer = 'yes';
