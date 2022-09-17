236f07939408e21c4859923c53309dfae0410fc7
Radgab <radgab712@gmail.com>
2022-09-17 10:59:34 +0300

    fix ../src/games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,7 +3,7 @@ import { getRndInteger } from '../Helper.js';
 const prime = () => {
   let correctAnswer = '';
   // eslint-disable-next-line prefer-const
-  let randomNumber = getRndInteger(100, 1);
+  let randomNumber = getRndInteger(100, 2);
   if (randomNumber % 2 === 0 || randomNumber % 3 === 0 || randomNumber % 5 === 0) {
     correctAnswer = 'no';
   } else {
