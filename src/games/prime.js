bd55c6b20ef10d8df4dac5e2aefe74c5a45bd1d1
Radgab <radgab712@gmail.com>
2022-09-17 09:37:28 +0300

    fix src/games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,7 +2,7 @@ import {getRndInteger} from '../Helper.js';
 
 const  prime = () => {
     let correctAnswer = '';
-    let randomNumber = getRndInteger(100, 1)
+    let randomNumber = getRndInteger(100, 1);
     if  ( randomNumber % 2 === 0 || randomNumber % 3 === 0 ) {
       correctAnswer = 'no';
     }else{
