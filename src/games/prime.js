4823aa11edaf65eb1316f2f73e02018f692fa35a
Radgab <radgab712@gmail.com>
2022-10-22 00:59:59 +0300

    fix prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,19 +2,23 @@ import number from '../Helper.js';
 import gameBrainLogic from '../index.js';
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no"';
-const startGame = (randomNumber) => {
-  let correctAnswer = '';
-  if (randomNumber % 2 === 0 || randomNumber % 3 === 0
-    || randomNumber % 5 === 0 || randomNumber % 7 === 0) {
-    correctAnswer = 'no';
-  } else {
-    correctAnswer = 'yes';
+
+const isPrime = (randomNumber) => {
+  if (randomNumber < 2) {
+    return false;
   }
-  return [correctAnswer];
+  for (let i = 2; i <= Math.sqrt(randomNumber); i += 1) {
+    if (randomNumber % i === 0) {
+      return false;
+    }
+  }
+  return true;
 };
-const prime = () => {
+
+const sayPrimeOrNot = () => {
   const randomNumber = number(100, 2);
-  const result = startGame(randomNumber);
-  return [result[0], randomNumber];
+  const text = isPrime(randomNumber) ? 'yes' : 'no';
+  return [text, randomNumber];
 };
-export default () => gameBrainLogic(prime, gameDescription);
+
+export default () => gameBrainLogic(sayPrimeOrNot, gameDescription);
