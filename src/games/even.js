82546c28778051571ace8fdbc5f4bfed6ecdb937
Radgab <radgab712@gmail.com>
2022-12-06 12:25:33 +0300

    fix even.js

--- src/games/even.js
+++ src/games/even.js
@@ -2,7 +2,7 @@ import number from '../Helper.js';
 import gameBrainLogic from '../index.js';
 
 const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
-const isSimpleNumber = (randomNumber) => {
+const SimpleNumber = (randomNumber) => {
   if (randomNumber % 2 === 0) {
     return 'yes';
   }
@@ -10,7 +10,7 @@ const isSimpleNumber = (randomNumber) => {
 };
 const evenNumbers = () => {
   const randomNumber = number(10, 0);
-  const correctAnswer = isSimpleNumber(randomNumber);
+  const correctAnswer = SimpleNumber(randomNumber);
   return [correctAnswer, randomNumber];
 };
 export default () => gameBrainLogic(evenNumbers, gameDescription);
