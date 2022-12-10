7e86b8274fae59738700ea245cdc724fc7a804f3
Radgab <radgab712@gmail.com>
2022-12-10 18:31:03 +0300

    fix src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,28 +4,25 @@ import gameBrainLogic from '../index.js';
 const gameDescription = 'What number is missing in the progression?';
 const algebraicProgressions = (firstNumber, step) => {
   const progressionLength = 7;
-  const algebraicProgression = [firstNumber];
+  const algebraicProgression = [];
   for (let i = 0; algebraicProgression.length - 1 < progressionLength; i += 1) {
-    algebraicProgression[i] += step;
-    algebraicProgression.push(algebraicProgression[i]);
+    algebraicProgression.push(firstNumber + i * step);
   }
-  algebraicProgression.pop();
   return algebraicProgression;
 };
-const startGame = (firstNumber, step) => {
+const startGame = (firstNumber, step, randomPlaceQuotes) => {
   const algebraicProgression = algebraicProgressions(firstNumber, step);
-  const randomPlaceQuotes = 6;
-  let resultNumber = '';
-  const e = Math.floor(Math.random() * randomPlaceQuotes) + 1;
-  resultNumber += algebraicProgression[e];
-  algebraicProgression[e] = '..';
+  const resultNumber = algebraicProgression[randomPlaceQuotes];
+  algebraicProgression[randomPlaceQuotes] = '..';
   const question1 = `${algebraicProgression.join(' ')}`;
   return [resultNumber, question1];
 };
 const progression = () => {
   const firstNumber = number(100, 1);
   const step = number(5, 1);
-  const result = startGame(firstNumber, step);
-  return [result[0], result[1]];
+  const randomPlaceQuotes = number(6, 1);
+  const result = startGame(firstNumber, step, randomPlaceQuotes);
+  const rightAnswer = String(result[0]);
+  return [rightAnswer, result[1]];
 };
 export default () => gameBrainLogic(progression, gameDescription);
